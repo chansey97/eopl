7ad674f14aebb22ea006cba1e07d6a41631bd5ea
chansey <chansey97@126.com>
2020-10-05 00:55:48 +0800

    Move `basic-letcc`, `basic-letcc-print` and `basic-letcc-print-set!` from `cps` folder to `cps-side-effects` folder, because they are 2 different sections of chapter8.

--- 2ed/chapter8/cps-side-effects/letcc-print/examples/test.scm
+++ 2ed/chapter8/cps-side-effects/letcc-print/examples/test.scm
@@ -1,10 +1,9 @@
 (load "../../../../poor-man-module.scm")
 (load-module "../../../../libs/" "r5rs")
-(load-module "../../../../libs/" "pmatch")
 (load-module "../../../../libs/" "sllgen")
 (load-module "../../../../libs/" "define-datatype")
 (load-module "../../../../libs/" "test-harness")
 
-(load-module "../../" "basic-letcc-print-set!")
+(load-module "../../" "letcc-print")
 
-(cps '(lambda (x) x))
+(cps "proc (x) x")
--- 2ed/chapter8/cps-side-effects/letcc/examples/test.scm
+++ 2ed/chapter8/cps-side-effects/letcc/examples/test.scm
@@ -1,10 +1,9 @@
 (load "../../../../poor-man-module.scm")
 (load-module "../../../../libs/" "r5rs")
-(load-module "../../../../libs/" "pmatch")
 (load-module "../../../../libs/" "sllgen")
 (load-module "../../../../libs/" "define-datatype")
 (load-module "../../../../libs/" "test-harness")
 
-(load-module "../../" "basic-letcc-print")
+(load-module "../../" "letcc")
 
-(cps '(lambda (x) x))
+(run "proc (x) x")
